d360ee62462fd458d4c55e275690c55429ab8b21
Juanda9919 <juanda-9919@hotmail.com>
2025-04-03 22:43:54 -0500

    Page worKing. Fix to register page

--- src/main/java/com/prod/LBU/controllers/DumbController.java
+++ src/main/java/com/prod/LBU/controllers/DumbController.java
@@ -1,8 +1,8 @@
 package com.prod.LBU.controllers;
 
-import com.prod.LBU.models.User;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
--- src/main/java/com/prod/LBU/controllers/ViewController.java
+++ src/main/java/com/prod/LBU/controllers/ViewController.java
@@ -1,7 +1,6 @@
 package com.prod.LBU.controllers;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +15,19 @@ public class ViewController {
         model.addAttribute("picture", oAuth2AuthenticationToken.getPrincipal().getAttribute("picture"));
         return "user-profile";
     }
+    @GetMapping("/complete-profile")
+    public String showCompleteProfileForm(OAuth2AuthenticationToken token, Model model) {
+        if (token != null) {
+            model.addAttribute("name", token.getPrincipal().getAttribute("name"));
+            model.addAttribute("email", token.getPrincipal().getAttribute("email"));
+        }
+        return "complete-profile";
+    }
 
     //TODO tienes que crear otro view con un template que permita a la persona que se está registrando llenar el resto de sus datos
+    //ya casi terminas recuerda que tienes que arreglar el .html
+
+
     //TODO averigua cómo despues de hacer un register podemos conectar un boton el front con la logica de salvar archivos
     //TODO deja de utilizar H2 vuelve a descargar postgres y crea una base de datos seria.
 }
